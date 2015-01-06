6f0d12e1ddf9d41d0d78468e4dc9d07c6f4a16b7
Marek Suscak <marek.suscak@gmail.com>
2015-01-06 22:55:10 +0100

    Update NavBar styles.

--- src/scripts/components/NavBar.jsx
+++ src/scripts/components/NavBar.jsx
@@ -4,14 +4,21 @@ var React = require('react');
 
 var NavBar = React.createClass({
   render: function() {
+    /*jshint ignore:start */
     return (
-      /*jshint ignore:start */
-      <div className="navbar dark fill-darken2 z10">
-        <input type="text" className="search-box unborder" />
-        <button className="register-button small strong unround"><i className="fa fa-plus fa-fw"/></button>
-      </div>
-      /*jshint ignore:end */
+      <nav className="navbar-wrapper z10 clearfix">
+        <div className="navbar-overlay fl dark fill-darken2">
+          <a href="#">
+            <h1 className="fl">Founders<strong>Map</strong></h1>
+          </a>
+          <div className="navbar fr">
+            <a className="nav-search button small strong unround"><i className="fa fa-search"/></a>
+          </div>
+        </div>
+        <a className="nav-register fr button small strong round-right">Registration</a>
+      </nav>
     );
+    /*jshint ignore:end */
   }
 });
 
