04b28aaa7b6b9d442bc02cc6b77d2710efac247b
Marek Suscak <marek.suscak@gmail.com>
2015-01-08 00:07:12 +0100

    Show data on the map when new props are received.

--- src/scripts/components/MapPane.jsx
+++ src/scripts/components/MapPane.jsx
@@ -15,10 +15,16 @@ var MapPane = React.createClass({
 
   componentDidMount: function() {
     this.bindMap();
+    this.showFounders(this.props.allFounders);
   },
 
   componentWillUnmount: function() {
     this.unbindMap();
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    this.clearFounders();
+    this.showFounders(nextProps.allFounders);
   },
 
   shouldComponentUpdate: function() {
@@ -41,6 +47,55 @@ var MapPane = React.createClass({
 
   unbindMap: function() {
     this._map.remove();
+  },
+
+  clearFounders: function() {
+    this._map.removeLayer(this._markers);
+    this._markers = null;
+    this._markersIndex = null;
+  },
+
+  showFounders: function(founders) {
+    var markers = new L.MarkerClusterGroup({ animateAddingMarkers: true });
+    var markersIndex = [];
+
+    founders.forEach(function(founder) {
+      var marker = L.marker(new L.LatLng(founder.latitude, founder.longitude), {
+          icon: L.mapbox.marker.icon({'marker-symbol': 'rocket', 'marker-color': '3498DB'}),
+          title: founder.companyName
+      });
+
+      // index it so we can focus the node later
+      markersIndex[founder.id] = marker;
+
+      marker.bindPopup(founder.companyName);
+      markers.addLayer(marker);
+    });
+
+    // bring marker to the center on click
+    markers.on('click', function(e) {
+      this._map.panTo(e.layer.getLatLng());
+    }.bind(this));
+
+    this._markers = markers;
+    this._markersIndex = markersIndex;
+    this._map.addLayer(markers);
+  },
+
+  focusFounderMarker: function(id) {
+    var markerLayer = this._markersIndex[id];
+    var matchingFounders = this.props.allFounders.filter(function(founder) { return founder.id === id });
+
+    if(!markerLayer || matchingFounders.length === 0) {
+      return;
+    }
+
+    var founder = matchingFounders[0];
+
+    this._map.panTo([founder.latitude, founder.longitude]);
+    this._markers.zoomToShowLayer(markerLayer, function() {
+      markerLayer.openPopup();
+    });
   },
 
   render: function() {
