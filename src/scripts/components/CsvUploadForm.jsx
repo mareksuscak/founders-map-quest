aa94827dda908b0a3885bce0ea4753c6c8bafa13
Marek Suscak <marek.suscak@gmail.com>
2015-01-12 00:12:45 +0100

    Finish the design of registration section.

--- src/scripts/components/CsvUploadForm.jsx
+++ src/scripts/components/CsvUploadForm.jsx
@@ -1,7 +1,8 @@
 'use strict';
 
 var React = require('react'),
-    cx = require('react/lib/cx'); // jshint ignore:line
+    cx = require('react/lib/cx'), // jshint ignore:line
+    ReactSelect = require('react-select'); // jshint ignore:line
 
 var CsvUploadForm = React.createClass({
 
@@ -14,6 +15,11 @@ var CsvUploadForm = React.createClass({
     this.props.onNextStep();
   },
 
+
+  handleSeparatorChange: function() {
+
+  },
+
   render: function() {
     /*jshint ignore:start */
     var containerClasses = cx({
@@ -24,9 +30,25 @@ var CsvUploadForm = React.createClass({
 
     return (
       <div className={containerClasses}>
+        <textarea placeholder="Paste founders CSV here..." className="row4 space-bottom1 col12"/>
+
         <div className="space-bottom1 clearfix">
-          <textarea placeholder="Paste founders CSV here..." className="row4 col12"/>
+          <div className="option col4">
+            <input type="radio" name="separator" id="comma" value="comma" onChange={this.handleSeparatorChange} />
+            <label className="inline" htmlFor="comma">Comma</label>
+          </div>
+
+          <div className="option col4">
+            <input type="radio" name="separator" id="semicolon" value="semicolon" onChange={this.handleSeparatorChange} />
+            <label className="inline" htmlFor="semicolon">Semicolon</label>
+          </div>
+
+          <div className="option col4">
+            <input type="radio" name="separator" id="tab" value="tab" onChange={this.handleSeparatorChange} />
+            <label className="inline" htmlFor="tab">Tab</label>
+          </div>
         </div>
+
         <div className="clearfix">
           <button className="col12" onClick={this.handleNextStepClick}>Next step</button>
         </div>
--- src/scripts/components/FieldMappingForm.jsx
+++ src/scripts/components/FieldMappingForm.jsx
@@ -15,6 +15,10 @@ var FieldMappingForm = React.createClass({
     this.props.onNextStep();
   },
 
+  handleCancelClick: function(e) {
+    e.preventDefault();
+  },
+
   render: function() {
     /*jshint ignore:start */
     var containerClasses = cx({
@@ -25,8 +29,23 @@ var FieldMappingForm = React.createClass({
 
     return (
       <div className={containerClasses}>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Company Name column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Latitude column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Longitude column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Founders column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Photo column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Homepage column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Street column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select City column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Postal Code column..."/>
+        <ReactSelect className="space-bottom0 col12" placeholder="Select Country column..."/>
+
         <div className="clearfix">
           <button className="col12" onClick={this.handleFinishClick}>Finish</button>
+        </div>
+
+        <div>
+          <small>or <a href="#" onClick={this.handleCancelClick}>Cancel</a></small>
         </div>
       </div>
     );
